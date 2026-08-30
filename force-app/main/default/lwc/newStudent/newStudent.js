6be7d691fa725e50d4dba2a5b28f0d8bda45df21
Akku <alexkukharev03@gmail.com>
2026-08-31 00:55:09 +0200

    Fix EMSO validation

--- force-app/main/default/lwc/newStudent/newStudent.js
+++ force-app/main/default/lwc/newStudent/newStudent.js
@@ -2,21 +2,27 @@
  * Created by Kevin on 28. 02. 24.
  */
 
-import {LightningElement, track} from 'lwc';
+import { LightningElement, track } from 'lwc';
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import createStudent from "@salesforce/apex/NewStudentController.createStudent";
 
 export default class NewStudent extends LightningElement {
     @track student = {};
     enrollmentTypeOptions = [
-        {label: "Full-Time", value: "Full-Time"},
-        {label: "Part-Time", value: "Part-Time"}
+        { label: "Regular Student", value: "Regular Student" },
+        { label: "Part-time Student", value: "Part-time Student" }
     ];
 
     get showPayerField() {
-        return this.student.enrollmentType === 'Part-Time';
+        return this.student.enrollmentType === 'Part-time Student';
     }
 
+    handleEnrollmentTypeChange(event) {
+        this.student = {
+            ...this.student,
+            enrollmentType: event.detail.value
+        };
+    }
 
     handleCreateStudent() {
         let fields = this.template.querySelectorAll('[data-input]');
@@ -25,7 +31,6 @@ export default class NewStudent extends LightningElement {
             let field = fields[i];
             this.student[field.dataset.input] = field.type == 'checkbox' ? field.checked : field.value;
         }
-
 
         createStudent({ studentJSON: JSON.stringify(this.student) })
             .then((result) => {
@@ -45,15 +50,11 @@ export default class NewStudent extends LightningElement {
             });
     }
 
-
-
     clearFields = (fieldList) => {
         [...fieldList].reduce((validSoFar, inputCmp) => {
             inputCmp.value = null;
             inputCmp.checked = false;
-            }, true);
+        }, true);
         this.student = {};
     };
-
-
 }
